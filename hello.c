7b44863e7ec2337972fbb116d11173c050c6c653
monalishajena <monalishajena662@gmail.com>
2023-12-02 16:46:01 +0530

    saved

--- hello.c
+++ hello.c
@@ -1,18 +1,5 @@
 #include<stdio.h>
 
 int main() {
-    int term,x=0,y=1,i=3,z;
-    printf("enter the term you want to print");
-    scanf("%d",&term);
-    z=x+y;
-    printf("%d%d%d",x,y,z);
-    while(i<=term) 
-    {
-        x=y;
-        y=z;
-        z=x+y;
-        printf("%d\t",z);
-        i++;
-    }
-    return 0;
+ printf("hello world");
 }
