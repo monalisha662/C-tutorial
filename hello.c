db2749950a52473f4e4d4f26ea42a9c804d2ea69
monalishajena <monalishajena662@gmail.com>
2023-12-08 14:21:59 +0530

    changed saved

--- hello.c
+++ hello.c
@@ -1,5 +1,24 @@
 #include<stdio.h>
 
 int main() {
- printf("hello world");
+ int i,j,k=1;
+ for(i=1;i<=5;i++)
+ {
+    for(j=1;j<=i;j++)
+    {
+        printf("*");
+        
+    }
+    printf("\n");
+ }
+    for(i=1;i<=5;i++)
+    {
+        for(k=4;k>=i;k--)
+        {
+        printf("*");
+    }
+    printf("\n");
+    }
+ 
+ return 0;
 }
